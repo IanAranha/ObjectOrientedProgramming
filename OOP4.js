8fe6baa6d44a650ce43bc0e9d81927ae17eca3cd
Ian Aranha <ianorama@gmail.com>
2018-11-12 10:43:51 -0800

    Object constructor functions.

---
+++ OOP4.js
@@ -0,0 +1,13 @@
+//Using the dog object, give it a method called sayLegs. The method should return the sentence "This dog has 4 legs."
+
+
+let dog = {
+    name : "Cosette", 
+    numLegs : 4,
+    sayLegs : function() {return "This dog has " + this.numLegs + " legs."}
+    
+}
+
+console.log("Dog's name: ", dog.name);
+console.log("Number of legs: ", dog.numLegs);
+console.log(dog.sayLegs())
