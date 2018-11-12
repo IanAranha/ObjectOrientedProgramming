56945edf6a00512db0581b0738306d5231a94ed1
Ian Aranha <ianorama@gmail.com>
2018-11-11 21:43:44 -0800

    Creating an object with properties

---
+++ OOP1.js
@@ -0,0 +1,10 @@
+//Create a dog object and give it two properties NAME and numLegs and give them a string value and number value respectively.
+
+
+
+let dog = {
+    name : "Cosette",
+    numLegs : 4
+}
+
+console.log(dog);
