d9490f9be3ed7a7e36bdad1740ed620beb07e96e
Ian Aranha <ianorama@gmail.com>
2018-11-12 10:52:30 -0800

    Passing values as a parameter of a constructor function.

---
+++ OOP7.js
@@ -0,0 +1,11 @@
+//Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
+
+function Dog(){
+    this.name = "Cosette";
+    this.color = "Golden";
+    this.numLegs = 4;
+}
+
+let hound = new Dog();
+
+console.log(hound.name);
