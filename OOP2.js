aa25b9c2a6b21eea850cffd2d353868085ede681
Ian Aranha <ianorama@gmail.com>
2018-11-11 21:44:20 -0800

    Dot notation to access object properties.

---
+++ OOP2.js
@@ -0,0 +1,9 @@
+//Print both properties of the dog object below to your console.l
+
+let dog = {
+    name : "Cosette", 
+    numLegs : 4
+}
+
+console.log("Dog's name: ", dog.name);
+console.log("Number of legs: ", dog.numLegs);
